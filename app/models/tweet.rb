40cc2e2f21b2b4b5f778f24a81429acb1aa229de
yama0603guchi <yamaguchi0603@icloud.com>
2022-02-16 12:53:04 +0900

    ルーティング設定

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,7 @@
 class Tweet < ApplicationRecord
+  validates :name, presence: true
+  validates :material, presence: true
+  validates :make, presence: true
+  validates :price, presence: true
+  validates :category_id, presence: true
 end
