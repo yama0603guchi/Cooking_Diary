3d90abae8e591cd67e1939899c1e606a566a1df5
yama0603guchi <yamaguchi0603@icloud.com>
2022-02-17 11:46:30 +0900

    デプロイ

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -11,6 +11,20 @@ class TweetsController < ApplicationController
     Tweet.create(tweet_params)
   end
 
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+  end
+
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    tweet.update(tweet_params)
+  end
+
   private
   def tweet_params
     params.require(:tweet).permit(:name, :image, :material, :make, :category_id, :price)
